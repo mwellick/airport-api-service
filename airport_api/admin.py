1f019c1a9ad6c9152607ad5778b413169f12acb3
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-05-13 12:12:17 +0300

    Optimized django admin panel and created superuser

--- airport_api/admin.py
+++ airport_api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import (
+    Crew,
+    Airport,
+    Route,
+    AirplaneType,
+    Airplane,
+    Flight,
+    Order,
+    Ticket
+)
 
-# Register your models here.
+admin.site.register(Crew)
+admin.site.register(Airport)
+admin.site.register(Route)
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Flight)
+admin.site.register(Order)
+admin.site.register(Ticket)
