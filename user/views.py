b99213f32894ad797ce94f91717bf9caae5ac77f
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-05-16 16:06:18 +0300

    Created user detail serialzier,view and endpoint

--- user/views.py
+++ user/views.py
@@ -1,6 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
 from .serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
