ff6351e5886e6fb9bfc28e1ee3b73a0bb287803d
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-05-16 13:54:55 +0300

    Added IsAuthenticated permission class for project

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -132,7 +132,10 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 5,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 
