ac9bf9d4bbb62ee1a593987dd9db3c91ba66f2cb
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-05-16 12:20:16 +0300

    Created user serializer,view and register endpoint

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
     path("api/airport/", include("airport_api.urls", namespace="api_airport"))
 ]
