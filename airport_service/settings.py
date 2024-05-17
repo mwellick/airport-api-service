71a7dc3d3bfe5fa52bc470f095d820faf98659e1
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-05-18 00:50:29 +0300

    Added default throttle classes and throttle rates for project

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -161,4 +161,12 @@ SPECTACULAR_SETTINGS = {
         "defaultModelExpandDepth": 2,
         "defaultModelsExpandDepth": 2,
     },
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
